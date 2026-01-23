f1a294fff2f51513d5bc2e110be5e5d2c56b7547
Fernanda Souza Oliveira <fernanda.oliveira@dbserver.com.br>
2026-01-23 08:40:55 -0300

    Entidade Aluguel

--- src/main/java/com/db/api_biblioteca/domain/entity/Aluguel.java
+++ src/main/java/com/db/api_biblioteca/domain/entity/Aluguel.java
@@ -1,4 +1,50 @@
 package com.db.api_biblioteca.domain.entity;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 public class Aluguel {
+    private LocalDate dataRetirada;
+    private LocalDate dataDevolucao;
+    private ArrayList<Livro> livros;
+    private ArrayList<Locatario> locatarios;
+
+    public Aluguel(LocalDate dataRetirada, LocalDate dataDevolucao, ArrayList<Livro> livros, ArrayList<Locatario> locatarios) {
+        this.dataRetirada = dataRetirada;
+        this.dataDevolucao = dataDevolucao;
+        this.livros = livros;
+        this.locatarios = locatarios;
+    }
+
+    public LocalDate getDataRetirada() {
+        return dataRetirada;
+    }
+
+    public void setDataRetirada(LocalDate dataRetirada) {
+        this.dataRetirada = dataRetirada;
+    }
+
+    public LocalDate getDataDevolucao() {
+        return dataDevolucao;
+    }
+
+    public void setDataDevolucao(LocalDate dataDevolucao) {
+        this.dataDevolucao = dataDevolucao;
+    }
+
+    public ArrayList<Livro> getLivros() {
+        return livros;
+    }
+
+    public void setLivros(ArrayList<Livro> livros) {
+        this.livros = livros;
+    }
+
+    public ArrayList<Locatario> getLocatarios() {
+        return locatarios;
+    }
+
+    public void setLocatarios(ArrayList<Locatario> locatarios) {
+        this.locatarios = locatarios;
+    }
 }
